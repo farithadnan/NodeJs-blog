cf0c5334bdae9cc17e63ed54035ef2a90ff22e62
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-10-14 21:19:39 +0800

    Get data from db for post

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,18 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post')
 
 
 
 
-// Routes
-router.get('', (req, res) => {
-    const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
+/**
+ * GET /
+ * HOME
+ */
+router.get('', async (req, res) => {
+    try {
+      const locals = {
+          title: "NodeJs Blog",
+          description: "Simple Blog created with NodeJs, Express & MongoDb."
+      }
+
+      let perPage = 6;
+      let page = req.query.page || 1;
+
+      const data = await Post.aggregate([ { $sort: { createdAt: -1 }}])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+      const count = await Post.countDocuments();
+      const nextPage = parseInt(page) + 1;
+      const hasNextPage = nextPage <= Math.ceil(count / perPage);
+ 
+      res.render('index', { 
+        locals, 
+        data,
+        current: page, 
+        nextPage: hasNextPage ? nextPage : null
+      });
+
+    } catch (error) {
+      console.log(error);
     }
-
-    res.render('index', { locals });
 });
+
+
 
 router.get("/about", (req, res) => {
   res.render("about");
